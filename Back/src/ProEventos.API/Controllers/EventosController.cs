108ad8574063834d0d646dfd601ab830b52bef0b
mgfiorentin <matheus.fiorentin@gmail.com>
2022-06-24 13:46:57 -0300

    Iniciando angular

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -13,12 +13,12 @@ namespace ProEventos.API.Controllers
 
     [ApiController]
     [Route("api/[controller]")]
-    public class EventoController : ControllerBase
+    public class EventosController : ControllerBase
     {
         
         private readonly DataContext _context;
 
-        public EventoController(DataContext context)
+        public EventosController(DataContext context)
         {
             _context = context;
         }
@@ -32,7 +32,8 @@ namespace ProEventos.API.Controllers
         [HttpGet("{id}")]
         public Evento GetById(int id)
         {
-            return _context.Eventos.FirstOrDefault(e => e.EventoId == id);
+            return _context.Eventos.FirstOrDefault
+            (e => e.EventoId == id);
         }
     }
 }
