40d2fde2c310f4c4004cb9834405cc84c420fbf6
mgfiorentin <matheus.fiorentin@gmail.com>
2022-06-24 18:21:38 -0300

    Exibindo e filtrando eventos

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,22 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-
+  hideImg = true;
   public eventos: any;
+  widthImg = 150;
+  marginImg = 1;
+  private _filtroLista: string= '';
+  public eventosFiltrados:any;
+
+  public get filtroLista():string {return this._filtroLista}
+
+  public set filtroLista(filtroLista:string ){
+    this._filtroLista=filtroLista;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) :this.eventos
+  }
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+
   }
 
+  public filtrarEventos(filtroLista:string): any {
+    filtroLista = filtroLista.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      (e:any)  => e.tema.toLocaleLowerCase().indexOf(filtroLista) !== -1 ||
+      e.local.toLocaleLowerCase().indexOf(filtroLista)!== -1
+    );
+  }
 
 public getEventos(): void {
 
-this.http.get('https://localhost:5001/api/Eventos/')
-.subscribe(response => this.eventos = response,
-
-  error => console.log(error));
+  this.http.get('https://localhost:5001/api/Eventos/')
+  .subscribe(response => {
+    this.eventos = response;
+    this.eventosFiltrados = this.eventos;
+  },
+    error => console.log(error));
 
 
 
